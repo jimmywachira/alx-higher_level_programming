da3a864f37dbe4a4cdae5ea1715b0702b87eae10
jimmywachira <jwachira40@gmail.com>
2024-01-23 09:28:09 -0800

    0x06-python_classes_objects

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
-"""A module for square"""
+"""Square module."""
+
 
 class Square:
-    """Represent a square."""
-    pass
+    """Defines a square."""
+
+    def __init__(self, size):
+        """Constructor.
+        Args:
+            size: length of side of the square.
+        """
+        self.__size = size
